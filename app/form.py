61bc64170e144a6382dc2282c1afd895e75743dc
SlinkEEE <babayans@oregonstate.edu>
2020-10-17 17:17:26 -0400

    adding new form fields

--- app/form.py
+++ app/form.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, IntegerField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class RiskForm(FlaskForm):
-    statename = StringField('Name of state', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+	states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+						'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+						'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+						'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+						'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+
+	activity1 = IntegerField('Gone grocery shopping', default=0)
+	activity2 = IntegerField('Walked, ran, biked, or sported outdoors with others', default=0)
+	activity3 = IntegerField('Ate at a restaurant (outdoors)', default=0)
+	statename = SelectField(
+					  	'Which U.S. state are you in?',
+					  	choices=[(i) for i in states],
+					  	validators=[DataRequired()])
+	submit = SubmitField('Calculate your risk')
