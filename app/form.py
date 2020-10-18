dc83e628c8b18b8ff15b63a8a175dd40cca9ab9c
SlinkEEE <babayans@oregonstate.edu>
2020-10-18 12:40:42 -0400

    Form styling improvements

--- app/form.py
+++ app/form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired
 
 class RiskForm(FlaskForm):
 	states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
@@ -9,11 +9,11 @@ class RiskForm(FlaskForm):
 						'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
 						'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 
-	activity1 = IntegerField('Gone grocery shopping', default=0)
-	activity2 = IntegerField('Walked, ran, biked, or sported outdoors with others', default=0)
-	activity3 = IntegerField('Ate at a restaurant (outdoors)', default=0)
+	activity1 = IntegerField('Gone grocery shopping', default=0, validators=[InputRequired()])
+	activity2 = IntegerField('Walked, ran, biked, or sported outdoors with others', default=0, validators=[InputRequired()])
+	activity3 = IntegerField('Ate at a restaurant (outdoors)', default=0, validators=[InputRequired()])
 	statename = SelectField(
 					  	'Which U.S. state are you in?',
 					  	choices=[(i) for i in states],
-					  	validators=[DataRequired()])
+					  	validators=[InputRequired()])
 	submit = SubmitField('Calculate your risk')
