0330f49d389296af57c469e0839dcec8c452c065
Travis Laxson <laxsont@oregonstate.edu>
2020-10-25 20:12:55 -0500

    Added state multiplier calculation

--- app/algo.py
+++ app/algo.py
@@ -11,14 +11,14 @@ def getData():
 
 	# Build URL query string
 	url = 'https://api.covidtracking.com/v1/states/' + state + '/current.json'
-	print(url)
+	#print(url)
 
 	#Get response and convert to json payload
 	response = requests.get(url)
 	json_response = response.json()
 
-	print(type(json_response))	# dictionary type
-	print(json_response)	# print json
+	#DEBUG: Print json
+	print(json_response)
 
 	#iterate through json payload
 	for key, value in json_response.items():
@@ -26,9 +26,18 @@ def getData():
 			state = value
 			print(state)
 		if key == 'positiveIncrease':
-			positiveIncrease = value
-			print(positiveIncrease)
+			positive_increase = value
+			print(positive_increase)
 		if key == 'totalTestResultsIncrease':
-			totalTestResultsIncrease = value
-			print(totalTestResultsIncrease)
-	return positiveIncrease
+			total_test_results_increase = value
+			print(total_test_results_increase)
+
+	# Calc the state multiplier
+	try:
+		state_score = round(float(positive_increase / total_test_results_increase), 2)
+		print(state_score)
+	except ZeroDivisionError:
+		state_score = 0
+		print(state_score)
+
+	return positive_increase, state_score
