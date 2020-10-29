16cb19f49b25c0e8d5d012ab160f57e8d69c47ae
Clayton Hadley <hadleyc@oregonstate.edu>
2020-10-29 13:00:25 -0700

    Update form.py

--- app/form.py
+++ app/form.py
@@ -3,12 +3,14 @@ from wtforms import StringField, IntegerField, SelectField, SubmitField
 from wtforms.validators import InputRequired
 
 class RiskForm(FlaskForm):
+	"""
 	states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
 						'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
 						'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
 						'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
 						'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-
+	"""
+	
 	activity1 = IntegerField('Gone grocery shopping', default=0, validators=[InputRequired()])
 	activity2 = IntegerField('Walked, ran, biked, or sported outdoors with others', default=0, validators=[InputRequired()])
 	activity3 = IntegerField('Ate at a restaurant (outdoors)', default=0, validators=[InputRequired()])
