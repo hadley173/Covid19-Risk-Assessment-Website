682ed1d00e1a50dc7fd66b5504022faa8842c34a
Clayton Hadley <hadleyc@oregonstate.edu>
2020-10-29 11:35:17 -0700

    Update form.py

--- app/form.py
+++ app/form.py
@@ -3,7 +3,7 @@ from wtforms import StringField, IntegerField, SelectField, SubmitField
 from wtforms.validators import InputRequired
 
 class RiskForm(FlaskForm):
-	states = ['aal', 'AAK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+	states = ['al', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
 						'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
 						'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
 						'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
