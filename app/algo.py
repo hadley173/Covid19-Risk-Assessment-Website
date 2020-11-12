6f796956cac646fb417826e29425bf849f8ccbc8
Clayton Hadley <hadleyc@oregonstate.edu>
2020-11-12 15:59:39 -0800

    bar chart implementation
    code converted to use Single API call.
    Rough bar chart implemented.

--- app/algo.py
+++ app/algo.py
@@ -27,16 +27,156 @@ def getData():
 					'ny','nc','nd','oh','ok','or','pa','ri', 'sc','sd','tn','tx','ut','vt','va','wa','wv','wi','wy']
 
 	# empty list to hold data returned from api call
-	stateList = []
+	stateList = {}
+	url = 'https://api.covidtracking.com/v1/states/current.json'
+	#stateList2 = requests.get(url).json
+	response = requests.get(url)
+	json_response = response.json()
 
+	#stateList2 = json_response
+	#for item in stateList2.items():
+		#print(item)
+
+	
+	for i in range(56):
+		stateList[i] = json_response[i].copy()
+		print(json_response[i])
+		#stateList2.append( State(json_response[i].state, json_response[i].positive, json_response[i].positive_increase, json_response[i].totalTestResultsIncrease, 
+		#json_response[i].inIcuCurrently, json_response[i].hospitalizedCurrently))
+	
+	print ("STATELIST2 OBJECT START")
+
+	#print(stateList2[0])
+	
+
+	#user_state = stateList2[0].get('state')
+	#print("test.GET: ", user_state)
+	#print(stateList2[0].state)
+	#print(stateList2)
+	# iterate through each state to populate list
+	#for key, value in json_response.items():
+		#stateList3.add( State(state, positive, positive_increase, totalTestResultsIncrease, inIcuCurrently, hospitalizedCurrently))
+
+	
+
+	form = RiskForm()
+
+	# Pull the user input for state and convert to lower case
+	user_state = RiskForm().statename.data.lower()
+	print(user_state)
+
+	state_score = 1
+	default_risk = 0.083
+	# Calc the state multiplier
+	all_states_pos = {}
+	all_states_pos_inc = {}
+	index = 0
+	count = 0
+	try:
+		for i in stateList:
+			for item in stateList[i].items():
+				#copying too much data, need to refactor this
+				if (stateList[i].get('positive') >= 0):
+					all_states_pos[count] = stateList[i].get('positive')
+				if (stateList[i].get('positiveIncrease') >= 0):
+					all_states_pos_inc[count] = stateList[i].get('positiveIncrease')
+				
+				if(stateList[i].get('state').lower() == user_state):
+					index = i
+					#print('Match Found: ', stateList[i].get('state').lower())
+					try:
+						state_score = round(float(stateList[i].get('positiveIncrease') / (stateList[i].get('totalTestResultsIncrease'))), 2)
+					except: 
+						state_score = 0.083
+						print('error getting state score')
+					icuCurrently = stateList[i].get('inIcuCurrently')
+					hospCurrently = stateList[i].get('hospitalizedCurrently')
+					#print("state score: ", state_score)	
+			count += 1			
+	except ZeroDivisionError:
+		state_score = default_risk  #average positive test rate for all states over one week was 8.3%
+		print("default state score: ", state_score)
+
+	#for i in range(56):
+		#for item in stateList[i].items():
+
+
+	# need to account for states that do not report this data
+	try:
+		if icuCurrently == None:
+			icuCurrently = 0
+		if hospCurrently == None:
+			hospCurrently = 0
+	except:
+		print("no icu and hosp data")
+	
+	for item in stateList[index].items():
+		print(item)
+
+	try:
+		print('all states pos: ', all_states_pos)
+		print('all states pos inc: ', all_states_pos_inc)
+		print('all states pos[index]: ', all_states_pos[index])
+		print('all states pos inc[index]: ', all_states_pos_inc[index])
+	except:
+		print("copy error")
+
+	if state_score <= 0:
+		state_score = default_risk
+	
+	# state risk score capped at 35%
+	if state_score >=0.35:
+		state_score = 0.35
+
+	low_risk_rate = .02 * (1 + state_score)
+	mod_risk_rate = .04  * (1 + state_score)
+	mod_high_risk_rate = .08  * (1 + state_score)
+	high_risk_rate = .16  * (1 + state_score)
+
+	low_risk_events = form.activity1.data + form.activity2.data + form.activity3.data + form.activity4.data + form.activity5.data + form.activity6.data + form.activity7.data
+	mod_risk_events = form.activity8.data + form.activity9.data + form.activity10.data
+	mod_high_risk_events = form.activity11.data + form.activity12.data + form.activity13.data + form.activity14.data + form.activity15.data
+	high_risk_events = form.activity16.data + form.activity17.data + form.activity18.data + form.activity19.data
+
+	print("low_risk_events", low_risk_events)
+	print("mod_risk_events", mod_risk_events)
+	print("mod_high_risk_events", mod_high_risk_events)
+	print("high_risk_events", high_risk_events)
+
+	base_score = 1 - (pow((1 - low_risk_rate), low_risk_events) * pow((1 - mod_risk_rate), mod_risk_events) * pow((1 - mod_high_risk_rate), mod_high_risk_events) * pow((1 - high_risk_rate), high_risk_events))
+	
+
+	if base_score == 0:
+		base_score = .01 * (1 + state_score)
+
+	# need to account for default state risk with no activities
+	# convert risk and state scores to % so they can be displayed to the user
+	risk_rating = round(float(base_score *100), 1)
+	state_score = round(float(state_score*100), 1)
+
+	
+	print("base score: ", base_score)
+	
+	#if index > 0:
+	#	index -= 1
+	print("index: ", index)
+
+	#return state_score, risk_rating, low_risk_events, mod_risk_events, mod_high_risk_events, high_risk_events, positive, positive_increase, inIcuCurrently, hospitalizedCurrently, totalTestResultsIncrease
+	return risk_rating, state_score, stateList, low_risk_events, mod_risk_events, mod_high_risk_events, high_risk_events, icuCurrently, hospCurrently, index, all_states_pos, all_states_pos_inc
+
+
+'''
 	# iterate through each state to populate list
 	for obj in stateCodes:
 		# Build URL query string
+		# pull from all states page instead
+		
 		url = 'https://api.covidtracking.com/v1/states/' + obj + '/current.json'
 
 		#Get response and convert to json payload
 		response = requests.get(url)
 		json_response = response.json()
+		#print(json_response)
 
 		state = json_response['state']
 		positive = json_response['positive']
@@ -76,10 +216,9 @@ def getData():
 		#print(state)
 		#print(positive)
 		# for bar graph
-		###############################################################################
 
-		###############################################################################
-	form = RiskForm()
+
+		form = RiskForm()
 
 	# Pull the user input for state and convert to lower case
 	user_state = RiskForm().statename.data.lower()
@@ -104,50 +243,10 @@ def getData():
 	except ZeroDivisionError:
 		state_score = default_risk  #average positive test rate for all states over one week was 8.3%
 		print("default state score: ", state_score)
+		###############################################################################
 
-	if state_score <= 0:
-		state_score = default_risk
-	
-	# state risk score capped at 35%
-	if state_score >=0.35:
-		state_score = 0.35
-
-	low_risk_rate = .02 * (1 + state_score)
-	mod_risk_rate = .04  * (1 + state_score)
-	mod_high_risk_rate = .08  * (1 + state_score)
-	high_risk_rate = .16  * (1 + state_score)
-
-	low_risk_events = form.activity1.data + form.activity2.data + form.activity3.data + form.activity4.data + form.activity5.data + form.activity6.data + form.activity7.data
-	mod_risk_events = form.activity8.data + form.activity9.data + form.activity10.data
-	mod_high_risk_events = form.activity11.data + form.activity12.data + form.activity13.data + form.activity14.data + form.activity15.data
-	high_risk_events = form.activity16.data + form.activity17.data + form.activity18.data + form.activity19.data
-
-	print("low_risk_events", low_risk_events)
-	print("mod_risk_events", mod_risk_events)
-	print("mod_high_risk_events", mod_high_risk_events)
-	print("high_risk_events", high_risk_events)
-
-	base_score = 1 - (pow((1 - low_risk_rate), low_risk_events) * pow((1 - mod_risk_rate), mod_risk_events) * pow((1 - mod_high_risk_rate), mod_high_risk_events) * pow((1 - high_risk_rate), high_risk_events))
-	
-
-	if base_score == 0:
-		base_score = .01 * (1 + state_score)
-
-	# need to account for default state risk with no activities
-	# convert risk and state scores to % so they can be displayed to the user
-	risk_rating = round(float(base_score *100), 1)
-	state_score = round(float(state_score*100), 1)
-
-	if(icuCurrently == None): icuCurrently = 0
-	if(hospCurrently == None): hospCurrently = 0
-	print("base score: ", base_score)
-	print('hospCurrently: ', hospCurrently)
-	print('icuCurrently: ', icuCurrently)
-	index -= 1
-	print("index: ", index)
-
-	#return state_score, risk_rating, low_risk_events, mod_risk_events, mod_high_risk_events, high_risk_events, positive, positive_increase, inIcuCurrently, hospitalizedCurrently, totalTestResultsIncrease
-	return risk_rating, state_score, stateList, low_risk_events, mod_risk_events, mod_high_risk_events, high_risk_events, user_state, icuCurrently, hospCurrently, index
+		###############################################################################
+'''
 
 '''
 	def __init__(self, date, state,	dataQualityGrade, death, deathConfirmed, deathIncrease, deathProbable,	
--- app/form.py
+++ app/form.py
@@ -27,11 +27,14 @@ class RiskForm(FlaskForm):
 	statename = SelectField(
 					  	u'Which U.S. state are you in?',
 						choices=[
-						('AL', 'Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkansas'), ('CA', 'California'), ('CO', 'Colorado'), ('CT', 'Connecticut'), ('DC', 'Washington DC'), ('DE', 'Delaware'), ('FL', 'Florida'), 
-						('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'), ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisianna'), ('ME', 'Maine'), 
-						('MD', 'Maryland'), ('MA', 'Massachusettes'), ('MI', 'Michigan'), ('MN', 'Minnisota'), ('MS', 'Mississippi'), ('MO', 'Missouri'), ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'), ('NH', 'New Hampshire'), 
-						('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'), ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), 
-						('SC', 'South Carolina'), ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VT', 'Vermont'), ('VA', 'Virginia'), ('WA', 'Washington'), ('WV', 'West Virginia'), ('WI', 'Wisconsin'), ('WY', 'Wyoming')],
+						('AK', 'Alaska'), ('AL', 'Alabama'), ('AR', 'Arkansas'), ('AS', 'American Samoa'), ('AZ', 'Arizona'), ('CA', 'California'), ('CO', 'Colorado'), 
+						('CT', 'Connecticut'), ('DC', 'Washington DC'), ('DE', 'Delaware'), ('FL', 'Florida'), ('GA', 'Georgia'), ('GU', 'Guam'), ('HI', 'Hawaii'), ('IA', 'Iowa'), ('ID', 'Idaho'), 
+						('IL', 'Illinois'), ('IN', 'Indiana'), ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisianna'), ('MA', 'Massachusettes'), ('MD', 'Maryland'), ('ME', 'Maine'), 
+						('MI', 'Michigan'), ('MN', 'Minnisota'), ('MO', 'Missouri'), ('MP', 'Nortern Mariana Islands'), ('MS', 'Mississippi'), 
+						('MT', 'Montana'), ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('NE', 'Nebraska'), ('NH', 'New Hampshire'), ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NV', 'Nevada'),  
+						('NY', 'New York'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'), ('PA', 'Pennsylvania'), ('PR', 'Puerto Rico'),
+						('RI', 'Rhode Island'), ('SC', 'South Carolina'), ('SD', 'South Dakota'), ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VA', 'Virginia'), 
+						('VI', 'Virgin Islands'), ('VT', 'Vermont'), ('WA', 'Washington'), ('WI', 'Wisconsin'), ('WV', 'West Virginia'), ('WY', 'Wyoming')],
 					  	validators=[InputRequired()])
 
 	submit = SubmitField('Calculate your risk')
