c2823ae29c9170bec2c7b7b8bf2d151c78a9a777
Clayton Hadley <hadleyc@oregonstate.edu>
2020-11-24 16:27:18 -0800

    refactored js code
    refactored code in results-charts.js file

--- app/static/js/results-charts.js
+++ app/static/js/results-charts.js
@@ -10,7 +10,22 @@ function render_charts(zipped_pos_total, zipped_pos_inc, user_state_specifics){
 
   // --------------- GAUGE CHART ---------------
   // get chart canvas
-  var ctx = document.getElementById("gaugeChart").getContext("2d");
+  create_guage_chart();
+  create_activity_pie_chart(user_state_specifics);
+  create_hosp_pie_chart(user_state_specifics);
+ 
+  //chartData_daily, chartData_total, labels_daily, labels_total = prepare_data_and_labels(zipped_pos_total, zipped_pos_inc);
+  var {chartData_daily, chartData_total} = prepare_data_and_labels(zipped_pos_total, zipped_pos_inc);
+
+  console.log('chartData_daily' + chartData_daily);
+  console.log('chartData_total' + chartData_total);
+  createBarChart('total-pos-chart', chartData_total, colorScale, colorRangeInfo);
+  createBarChart('daily-pos-chart', chartData_daily, colorScale, colorRangeInfo);
+
+}
+  
+function create_guage_chart(){
+    var ctx = document.getElementById("gaugeChart").getContext("2d");
   // get chart data
   var score = 0
   score += document.getElementById("score").innerHTML
@@ -39,9 +54,13 @@ function render_charts(zipped_pos_total, zipped_pos_inc, user_state_specifics){
     }
   });
 
+}
 
-  // Global parameters: do not resize the chart canvas
-
+  
+    
+  // --------------- PIE CHART 1 ---------------
+  // get chart canvas
+function create_activity_pie_chart(user_state_specifics){
   var low_risk_events = user_state_specifics["low_risk_events"];
   var mod_risk_events = user_state_specifics["mod_risk_events"];
   var mod_high_risk_events = user_state_specifics["mod_high_risk_events"];
@@ -49,50 +68,52 @@ function render_charts(zipped_pos_total, zipped_pos_inc, user_state_specifics){
   var total_events = 0
 
   total_events += (low_risk_events*2) + (mod_risk_events*4) + (mod_high_risk_events*8) + (high_risk_events*16)
-    
-  // --------------- PIE CHART 1 ---------------
-  // get chart canvas
-  try{
-    var ctx = document.getElementById("pieChart1").getContext("2d");
-    // create chart using the canvas
-    var chart = new Chart(ctx, {
-      type: 'pie',
-      data: {
-        datasets: [{
-          // displays each event type by percentage of total risk score to 2 decimals
-          data: [(Math.round(low_risk_events*2/total_events*(100))/100), (Math.round(mod_risk_events*4/total_events*(100))/100), (Math.round(mod_high_risk_events*8/total_events*(100))/100), 
-          (Math.round(high_risk_events*16/total_events*(100))/100)],
-          backgroundColor: ['rgba(88, 214, 141, 1)', 'rgba(255, 195, 0, 1)', 'rgba(255, 87, 51, 1)', 'rgba(199, 0, 57, 1)'],
-        }],
-        labels: [
-          'Low risk',
-          'Moderate risk',
-          'Moderate-High risk',
-          'High risk'
-        ]
-      },
-      options: {
-        cutoutPercentage: 50,
-        legend: {
-          display: true,
-          position: 'top',
-          align: 'start',
-          labels: {
-            boxWidth: 20,
-            fontSize: 14,
-            fontStyle: 'bold',
-          },
+    try{
+      var ctx = document.getElementById("pieChart1").getContext("2d");
+      // create chart using the canvas
+      var chart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+          datasets: [{
+            // displays each event type by percentage of total risk score to 2 decimals
+            data: [(Math.round(low_risk_events*2/total_events*(100))/100), (Math.round(mod_risk_events*4/total_events*(100))/100), (Math.round(mod_high_risk_events*8/total_events*(100))/100), 
+            (Math.round(high_risk_events*16/total_events*(100))/100)],
+            backgroundColor: ['rgba(88, 214, 141, 1)', 'rgba(255, 195, 0, 1)', 'rgba(255, 87, 51, 1)', 'rgba(199, 0, 57, 1)'],
+          }],
+          labels: [
+            'Low risk',
+            'Moderate risk',
+            'Moderate-High risk',
+            'High risk'
+          ]
         },
-        events: ['click'], // prevents a weird hover bug
-      },
-    });
-  }
-  catch(err){
-    console.log('No activities selected')
-  }
+        options: {
+          cutoutPercentage: 50,
+          legend: {
+            display: true,
+            position: 'top',
+            align: 'start',
+            labels: {
+              boxWidth: 20,
+              fontSize: 14,
+              fontStyle: 'bold',
+            },
+          },
+          events: ['click'], // prevents a weird hover bug
+        },
+      });
+    }
+    catch(err){
+      console.log('No activities selected')
+    }
+}
+
+
+
 
   // --------------- PIE CHART 2 ---------------
   // get chart canvas
+function create_hosp_pie_chart(user_state_specifics){
   try{
     var ctx = document.getElementById("pieChart2").getContext("2d");
     var hospitalized = user_state_specifics["hosp_currently"];
@@ -130,72 +151,66 @@ function render_charts(zipped_pos_total, zipped_pos_inc, user_state_specifics){
   catch(err) {
     console.log("no hospitalization data");
   }
+}
+
 
   // --------------- Bar CHART  --------------
   // need to copy data sent by routes.py into javascript variables
   // Source: https://medium.com/code-nebula/automatically-generate-chart-colors-with-chart-js-d3s-color-scales-f62e282b2b41
-  function createBarChart(chartId, chartData, colorScale, colorRangeInfo) {
-  // Grab chart element by id 
-  const chartElement = document.getElementById(chartId);
-  const dataLength = chartData.data.length;
-  // Create color array 
-  var COLORS = interpolateColors(dataLength, colorScale, colorRangeInfo);
-  // Create chart 
-  const myChart = new Chart(chartElement, {
-  type: 'bar',
-  data: {
-    labels: chartData.labels,
-    datasets: [
-      {
-        backgroundColor: COLORS,
-        hoverBackgroundColor: COLORS,
-        data: chartData.data
-      }
-    ],
-  },
-  options: {
-    scales: {
-      xAxes: [
+function createBarChart(chartId, chartData, colorScale, colorRangeInfo) {
+    // Grab chart element by id 
+    const chartElement = document.getElementById(chartId);
+    const dataLength = chartData.data.length;
+    // Create color array 
+    var COLORS = interpolateColors(dataLength, colorScale, colorRangeInfo);
+    // Create chart 
+    const myChart = new Chart(chartElement, {
+    type: 'bar',
+    data: {
+      labels: chartData.labels,
+      datasets: [
         {
-          ticks: {
-              fontSize: 11,
-          },
-      }],
-      yAxes: [{
-          ticks: {
-              fontSize: 14,
-          },
-      }],
-  },
-    responsive: true,
-    legend: {
-      display: false,
+          backgroundColor: COLORS,
+          hoverBackgroundColor: COLORS,
+          data: chartData.data
+        }
+      ],
     },
-    hover: {
-      onHover: function(e) {
-        var point = this.getElementAtEvent(e);
-        e.target.style.cursor = point.length ? 'pointer' : 'default';
+    options: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+                fontSize: 11,
+            },
+        }],
+        yAxes: [{
+            ticks: {
+                fontSize: 14,
+            },
+        }],
+    },
+      responsive: true,
+      legend: {
+        display: false,
+      },
+      hover: {
+        onHover: function(e) {
+          var point = this.getElementAtEvent(e);
+          e.target.style.cursor = point.length ? 'pointer' : 'default';
+        },
       },
     },
-  },
-    events: ['click'], // prevents a weird hover bug
-  });
-    return myChart;
-  }
-  // fetch total and daily positive cases from routing page
-  //var pos = JSON.parse('{{zipped_pos_total | tojson}}');
-  //var pos_inc = JSON.parse('{{zipped_pos_inc | tojson}}');
-  var pos = zipped_pos_total;
-  var pos_inc = zipped_pos_inc;
-  // sort the states by total positive cases in descending order
-  var pos_sorted = sortJsObject(pos);
-  // sort the states by daily positive cases in descending order
-  var pos_inc_sorted = sortJsObject(pos_inc);
-  // for debugging
-  console.log('pos: ' + pos);
-  console.log('pos_inc: ' + pos_inc);
-  console.log('pos_sorted: ' + pos_sorted);
-  // Sort total cases in Descending order and then find the corresponding state code
+      events: ['click'], // prevents a weird hover bug
+    });
+      return myChart;
+}
+
+
+
+ 
+
+  
   function get_bar_chart_data(dict, dict_sorted){
     var i;
     var data = [];
@@ -238,40 +253,73 @@ function render_charts(zipped_pos_total, zipped_pos_inc, user_state_specifics){
       data, 
       labels
         }
-    }
+  }
+
+
+
+  function prepare_data_and_labels(zipped_pos_total, zipped_pos_inc){
+    // fetch total and daily positive cases from routing page
+    var pos = zipped_pos_total;
+    var pos_inc = zipped_pos_inc;
+    // sort the states by total positive cases in descending order
+    var pos_sorted = sortJsObject(pos);
+    // sort the states by daily positive cases in descending order
+    var pos_inc_sorted = sortJsObject(pos_inc);
+
+    // for debugging
+    console.log('pos: ' + pos);
+    console.log('pos_inc: ' + pos_inc);
+    console.log('pos_sorted: ' + pos_sorted);
+
+    // Sort total cases in Descending order and then find the corresponding state code
     var {data, labels} = get_bar_chart_data(pos, pos_sorted);
     var data_total = data;
     var labels_total = labels;
     var {data, labels} = get_bar_chart_data(pos_inc, pos_inc_sorted);
     var data_daily = data;
     var labels_daily = labels;
-    console.log("data out func: " + data_total);
-    console.log("labels out func: " + labels_total);
-    console.log("data out func: " + data_daily);
-    console.log("labels out func: " + labels_daily);
+    console.log("data_total out func: " + data_total);
+    console.log("labels_total out func: " + labels_total);
+    console.log("data_daily out func: " + data_daily);
+    console.log("labels_daily out func: " + labels_daily);
+
     const chartData_total = {
-    labels: labels_total,
-    data: data_total,
+      labels: labels_total,
+      data: data_total,
     };
     const chartData_daily = {
-    labels: labels_daily,
-    data: data_daily,
+      labels: labels_daily,
+      data: data_daily,
     };
-    const colorScale = d3.interpolateInferno;
-    const colorRangeInfo = {
-    colorStart: 0,
-    colorEnd: 1,
-    useEndAsStart: false,
-  }; 
+
+    return {
+      chartData_daily,
+      chartData_total
+        }
+  }
+
+
+
+
+const colorScale = d3.interpolateInferno;
+const colorRangeInfo = {
+  colorStart: 0,
+  colorEnd: 1,
+  useEndAsStart: false,
+}; 
+
+
   //var colorScale = chart_color();
-  function calculatePoint(i, intervalSize, colorRangeInfo) {
+function calculatePoint(i, intervalSize, colorRangeInfo) {
     var { colorStart, colorEnd, useEndAsStart } = colorRangeInfo;
     return (useEndAsStart
     ? (colorEnd - (i * intervalSize))
     : (colorStart + (i * intervalSize)));
-  }
+}
+
+
   // Must use an interpolated color scale, which has a range of [0, 1] 
-  function interpolateColors(dataLength, colorScale, colorRangeInfo) {
+function interpolateColors(dataLength, colorScale, colorRangeInfo) {
     var { colorStart, colorEnd } = colorRangeInfo;
     var colorRange = colorEnd - colorStart;
     var intervalSize = colorRange / dataLength;
@@ -281,28 +329,28 @@ function render_charts(zipped_pos_total, zipped_pos_inc, user_state_specifics){
       colorPoint = calculatePoint(i, intervalSize, colorRangeInfo);
       colorArray.push(colorScale(colorPoint));
     }
-    return colorArray;
-  }  
-  createBarChart('total-pos-chart', chartData_total, colorScale, colorRangeInfo);
-  createBarChart('daily-pos-chart', chartData_daily, colorScale, colorRangeInfo);
+  return colorArray;
+}  
+
   //  Sorts dictionary by Value from smallest to largest
   // https://stackoverflow.com/questions/25500316/sort-a-dictionary-by-value-in-javascript
-  function sortJsObject(dict) {
-    var keys = [];
-    for(var key in dict) { 
-      keys[keys.length] = key;
-    }
-    var values = []; 
-    for(var i = 0; i < keys.length; i++) {
-        values[values.length] = dict[keys [i]];
-    }
-    var sortedValues = values.sort(sortNumber);
-    return sortedValues;     
+function sortJsObject(dict) {
+  var keys = [];
+  for(var key in dict) { 
+    keys[keys.length] = key;
   }
-    function sortNumber(a,b) {
-      return a - b;
-    }
+  var values = []; 
+  for(var i = 0; i < keys.length; i++) {
+      values[values.length] = dict[keys [i]];
   }
+  var sortedValues = values.sort(sortNumber);
+  return sortedValues;     
+}
+
+function sortNumber(a,b) {
+  return a - b;
+}
+
 
 
 
